b673344794cebdd20ec3befa95ff4e638efda32d
niranjan verma <niranjan.sosvns@gmail.com>
2018-12-09 00:55:42 +0530

    likesdislikeinangular6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// import component decorator
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+// title to this app.
   title = 'my-LikesDislikes-app';
+// declare variable for like and dislike counter
+  likeCount : number = 0;
+  dislikeCount : number = 0;
+  // constructor to initialize the variable
+  constructor() { }
+ // lifecycle hook to execute the things post component get created
+  ngOnInit() {
+  }
+// like counter
+ onClickLikeCount(): void {
+    this.likeCount++;
 }
+
+// dislike counter
+  onClickDislikeCount(): void {
+    this.dislikeCount++;
+    // like counter check to decrease the value corresponding to dislike
+    if(this.likeCount > 0) {
+    	this.likeCount--;
+    }
+ }
+  
+}
+
+
